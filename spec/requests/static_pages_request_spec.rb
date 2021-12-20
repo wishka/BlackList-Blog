cb98628c525fefe49f7fa9dbdbf47f50679456df
wishka <roucke@mail.ru>
2021-12-20 20:01:35 +0300

    refactoring page titles

--- spec/requests/static_pages_request_spec.rb
+++ spec/requests/static_pages_request_spec.rb
@@ -5,22 +5,22 @@ RSpec.describe "static_pages", type: :request do
     it "should get home" do
       get static_pages_home_path
       assert_response :success
-      assert_select "title", "Home
-| Blog with BlackList"
+      assert_select "title",
+"Blog with BlackList"
     end
 
     it "should get help" do
       get static_pages_help_path
       assert_response :success
-      assert_select "title", "Help
-| Blog with BlackList"
+      assert_select "title",
+"Blog with BlackList"
     end
 
     it "should get about" do
       get static_pages_about_path
       assert_response :success
-      assert_select "title", "About
-| Blog with BlackList"
+      assert_select "title",
+"Blog with BlackList"
     end
   end
 end
