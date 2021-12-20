946e6cfb1d10927a055b56dd381c8c5bd0009f77
wishka <roucke@mail.ru>
2021-12-20 19:28:58 +0300

    add rspec tests

---
+++ spec/requests/static_pages_request_spec.rb
@@ -0,0 +1,26 @@
+require 'rails_helper'
+
+RSpec.describe "static_pages", type: :request do
+  describe "GET /index" do
+    it "should get home" do
+      get static_pages_home_path
+      assert_response :success
+      assert_select "title", "Home
+| Blog with BlackList"
+    end
+
+    it "should get help" do
+      get static_pages_help_path
+      assert_response :success
+      assert_select "title", "Help
+| Blog with BlackList"
+    end
+
+    it "should get about" do
+      get static_pages_about_path
+      assert_response :success
+      assert_select "title", "About
+| Blog with BlackList"
+    end
+  end
+end
